a89b5f1edee2bd874c5e499f0db09cae5407105e
James Schull <jameskschull@gmail.com>
2019-11-25 20:14:49 +0000

    Revert "Task 1 (search for ingredient) functional"
    This reverts commit d76df8f509934ceaa091bce4ef339fd11c6319f0.

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,27 +1,18 @@
 import { createBottomTabNavigator } from 'react-navigation-tabs'
 import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
-import { createDrawerNavigator } from 'react-navigation-drawer';
-
 import React from 'react';
 import { Images, Colors, Metrics } from '../Themes'
 import { StyleSheet, Image } from 'react-native';
 import { Entypo } from '@expo/vector-icons';
 import HomeScreen from '../Screens/HomeScreen'
-import MessagesScreen from '../Screens/MessagesScreen'
-import IngredientSearchScreen from '../Screens/IngredientSearchScreen'
-import IngredientSearchResultScreen from '../Screens/IngredientSearchResultScreen'
+import BookmarkScreen from '../Screens/BookmarkScreen'
+import BookmarkViewerScreen from '../Screens/BookmarkViewerScreen'
+import UserProfileScreen from '../Screens/UserProfileScreen'
+
 
 /* PART 1: Create a StackNavigator that contains the HomeScreen, and the UserProfileScreen */
 /* initialRouteName should be your HomeScreen. Set headerMode to 'float'  */
-
-HomeStackNav = createStackNavigator({
-  Home: { screen: HomeScreen },
-  IngredientSearch: { screen: IngredientSearchScreen },
-  IngredientSearchResult: { screen: IngredientSearchResultScreen}
-}, {
-  initialRouteName: 'Home'
-})
 
 /* PART 2: Create a StackNavigator that contains the BookmarkScreen, and the BookmarkViewerScreen */
 /* initialRouteName should be your BookmarkScreen. Set headerMode to 'float'  */
@@ -45,49 +36,41 @@ BookmarkNav.navigationOptions = ({ navigation }) => {
 
 */
 
-// MessagesScreen.navigationOptions = ({ navigation }) => {
-//   return {
-//     tabBarLabel: 'Bookmarks',
-//     tabBarIcon: ({ tintColor }) => (
-//       <Entypo name="bookmark"
-//         size={Metrics.icons.medium}
-//         color={tintColor} />
-//     ),
-//   };
-// };
+BookmarkScreen.navigationOptions = ({ navigation }) => {
+  return {
+    tabBarLabel: 'Bookmarks',
+    tabBarIcon: ({ tintColor }) => (
+      <Entypo name="bookmark"
+        size={Metrics.icons.medium}
+        color={tintColor} />
+    ),
+  };
+};
 
-// HomeScreen.navigationOptions = ({ navigation }) => {
-//   return {
-//     tabBarLabel: 'Home',
-//     tabBarIcon: ({ tintColor }) => (
-//       <Entypo name="home"
-//         size={Metrics.icons.medium}
-//         color={tintColor} />
-//     ),
-//   };
-// };
+HomeScreen.navigationOptions = ({ navigation }) => {
+  return {
+    tabBarLabel: 'Home',
+    tabBarIcon: ({ tintColor }) => (
+      <Entypo name="home"
+        size={Metrics.icons.medium}
+        color={tintColor} />
+    ),
+  };
+};
 
 // Manifest of possible screens
-// const TabNav = createBottomTabNavigator({
-//   FeedScreen: { screen: HomeScreen },
-//   BookmarkScreen:   { screen: BookmarkScreen },
-// }, {
-//   // Default config for all screens
-//   initialRouteName: 'FeedScreen',
-//   tabBarOptions: {
-//     activeTintColor: Colors.black,
-//     showLabel: true,
-//   },
-// })
-
-const DrawerNav = createDrawerNavigator({
-  Home: { screen: HomeStackNav },
-  Messages: { screen: MessagesScreen},
+const TabNav = createBottomTabNavigator({
+  FeedScreen: { screen: HomeScreen },
+  BookmarkScreen:   { screen: BookmarkScreen },
 }, {
-  initialRouteName: 'Home',
-
+  // Default config for all screens
+  initialRouteName: 'FeedScreen',
+  tabBarOptions: {
+    activeTintColor: Colors.black,
+    showLabel: true,
+  },
 })
 
-const AppContainer = createAppContainer(DrawerNav);
+const AppContainer = createAppContainer(TabNav);
 
 export default AppContainer
--- App/Themes/Images.js
+++ App/Themes/Images.js
@@ -1,18 +1,6 @@
 // leave off @2x/@3x
 const images = {
   placeholder: require('../Images/placeholder.jpg'),
-  fruit: require('../Images/fruit.png'),
-  vegetables: require('../Images/vegetables.png'),
-  grains: require('../Images/grains.png'),
-  bookmark1: require('../Images/bkmark1.png'),
-  bookmark2: require('../Images/bkmark2.png'),
-  bookmark3: require('../Images/bkmark3.png'),
-  parmesan: require('../Images/parmesan.png'),
-  shishito: require('../Images/shishito.png'),
-  truffles: require('../Images/truffles.png'),
-  apple: require('../Images/apple.png'),
-  granny_smith: require('../Images/granny-smith.png'),
-  apples: require('../Images/apples.png')
 }
 
 export default images
